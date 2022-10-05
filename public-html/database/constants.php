27ffc04e39025e5aeaf919ef98736cea5498e4c4
MOUAD BOUSALLAM <64041348+Mouad-MB10@users.noreply.github.com>
2022-10-05 03:25:34 +0100

    Update constants.php

--- public-html/database/constants.php
+++ public-html/database/constants.php
@@ -5,6 +5,6 @@ define('HOST','eu-cdbr-west-03.cleardb.net');
 define('USER','b48af48f52de16');
 define('PASS','b17dc19b');
 define('DB','heroku_105386ae5d2bc46');
-define("DOMAIN","https://inventory-s-m.herokuapp.com/inventory/public-html");
+define("DOMAIN","http://localhost/inventoryinventory/public-html");
 
 ?>
