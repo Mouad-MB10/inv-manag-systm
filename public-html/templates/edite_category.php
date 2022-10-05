fd88ea31d8328b4b3bdfa6b781081c31ec3bf984
MOUAD BOUSALLAM <64041348+Mouad-MB10@users.noreply.github.com>
2022-10-05 17:52:37 +0100

    shsh

--- public-html/templates/edite_category.php
+++ public-html/templates/edite_category.php
@@ -27,7 +27,7 @@
                 </form>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" name="d">Close</button>
             </div>
         </div>
     </div>
